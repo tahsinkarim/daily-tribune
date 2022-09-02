3db4dff6cb2323fb5e13bef255899fb312586094
tahsinkarim <tahsinnkarim@gmail.com>
2022-09-02 22:26:16 +0600

    Added get news category function

--- js/app.js
+++ js/app.js
@@ -10,12 +10,22 @@ const displayCategories = data =>{
     data.forEach(element =>{
         const li = document.createElement('li')
         li.innerHTML = `
-            <button onclick="getCategory(${element.category_id})" class="fw-semibold item border-0">${element.category_name}</button>
+            <button onclick="getNewsCategory(${element.category_id})" class="fw-semibold item border-0">${element.category_name}</button>
         `
         listContainer.appendChild(li)
     })
 }
-const getCategory = id =>{
-    console.log(id)
+const getNewsCategory = async (id) =>{
+    if(id < 10){
+        id = '0' + id
+    }
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayNewsCategory(data.data)
+}
+
+const displayNewsCategory = data =>{
+    console.log(data)
 }
 loadData()
