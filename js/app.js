8a3cc4d3a638a0e2131fd215f784142a19a34db3
tahsinkarim <tahsinnkarim@gmail.com>
2022-09-03 00:14:18 +0600

    Card Section added

--- js/app.js
+++ js/app.js
@@ -10,22 +10,28 @@ const displayCategories = data =>{
     data.forEach(element =>{
         const li = document.createElement('li')
         li.innerHTML = `
-            <button onclick="getNewsCategory(${element.category_id})" class="fw-semibold item border-0">${element.category_name}</button>
+            <button onclick="getNewsCategory(${element.category_id}, '${element.category_name}')" class="fw-semibold item border-0">${element.category_name}</button>
         `
         listContainer.appendChild(li)
     })
 }
-const getNewsCategory = async (id) =>{
+const getNewsCategory = async (id , name) =>{
     if(id < 10){
         id = '0' + id
     }
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    displayNewsCategory(data.data)
+    displayNewsCategory(data.data, name)
 }
 
-const displayNewsCategory = data =>{
-    console.log(data)
+const displayNewsCategory = (data, name) =>{
+    const newsItemsCount = document.getElementById('news-items-count')
+    if (data.length === 0){
+        newsItemsCount.innerText = `No news found for category ${name}`
+    } else {
+        newsItemsCount.innerText = `${data.length} items found for category ${name}`
+    }
 }
+getNewsCategory(01, 'Breaking News')
 loadData()
